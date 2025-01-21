d1b5d9c09401b1da406df8644c560c4f64bf0b44
sergiohernandezitvx <sergio.hernandez.araujo@itv.com>
2025-01-21 14:15:09 +0000

    feat: added postgress db

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,76 @@
 import express, { Request, Response } from "express";
+import { Pool } from "pg";
 
 const app = express();
 const port = process.env.PORT;
+
+const pool = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.DB_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: Number(process.env.DB_PORT),
+});
+
+app.get("/db", async (_req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT NOW()");
+    console.info({ result });
+    console.info({
+      user: process.env.POSTGRES_USER,
+      host: process.env.DB_HOST,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+      port: Number(process.env.DB_PORT),
+    });
+    res.json(result.rows);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+// app.get("/db", async (_req, res) => {
+//   try {
+//     const client = await pool.connect();
+
+//     // Create the table if it doesn't exist
+//     await client.query(`
+//       CREATE TABLE IF NOT EXISTS users (
+//         id SERIAL PRIMARY KEY,
+//         name VARCHAR(100),
+//         email VARCHAR(100)
+//       );
+//     `);
+
+//     // Insert a record
+//     await client.query(`
+//       INSERT INTO users (name, email)
+//       VALUES ('John Doe', 'john.doe@example.com')
+//       ON CONFLICT DO NOTHING; -- Avoid inserting duplicate records
+//     `);
+
+//     // Retrieve the records from the table
+//     const result = await client.query("SELECT * FROM users");
+
+//     console.info({ result: result.rows });
+//     console.info({
+//       user: process.env.POSTGRES_USER,
+//       host: process.env.DB_HOST,
+//       database: process.env.POSTGRES_DB,
+//       password: process.env.POSTGRES_PASSWORD,
+//       port: Number(process.env.DB_PORT),
+//     });
+
+//     res.json(result.rows); // Return all records in the 'users' table
+//     client.release();
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Error: " + err);
+//   }
+// });
 
 app.get("/", (_req: Request, res: Response) => {
   res.send(`Sergio is cool ${port}`);
---
+++ src/types/pg.d.ts
@@ -0,0 +1,4 @@
+// declare module "pg";
+declare module "Pool" {
+  export = any; // Declare pg as 'any' type to bypass the error
+}
